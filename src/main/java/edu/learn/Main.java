e91dd38c2625af46cc5c680ac8dbb0c0053d22db
sanitar4eg <sanitar.92@gmail.com>
2014-11-24 03:17:08 +0400

    add maven

---
+++ src/main/java/edu/learn/Main.java
@@ -0,0 +1,14 @@
+package edu.learn;
+
+import edu.learn.reminder.ApplicationForReminder;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationForReminder.launch(args);
+	}
+
+}
